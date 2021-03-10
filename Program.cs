107ecd1ec1aee5d3caf23b80975bc01f40f55a4d
sanjana k <sanjanarthi@gmail.com>
2021-03-10 12:51:05 +0530

    "Added UC2 "

--- Program.cs
+++ Program.cs
@@ -6,8 +6,20 @@ namespace GameProblem
     {
         int cash = 100;
         int bet = 1;
+
         static void Main(string[] args)
         {
+
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == bet)
+            {
+                Console.WriteLine("win")
+            }
+            else
+            {
+                Console.WriteLine("loose")
+            }
             Console.WriteLine("welcome to game Problem");
         }
     }
